2e039bdc9915a4ab0d26f699b96ff197a6c4956c
Karawangasb <78271095+Karawangasb@users.noreply.github.com>
2025-10-21 08:29:06 +0700

    Update tokens.js

--- arbitrage2/tokens.js
+++ arbitrage2/tokens.js
@@ -1,13 +1,23 @@
 // tokens.js
 export const tokens = [
-  { name: "POL",   address: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", decimals: 18 },
-  { name: "USDT",  address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", decimals: 6 },
-  { name: "USDC",  address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", decimals: 6 },
-  { name: "DAI",   address: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", decimals: 18 },
-  { name: "WETH",  address: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", decimals: 18 },
-  { name: "WBTC",  address: "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6", decimals: 8 },
-  { name: "AAVE",  address: "0xD6DF932A45C0f255f85145f286eA0b292B21C90B", decimals: 18 },
-  { name: "SUSHI", address: "0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a", decimals: 18 },
-  { name: "QUICK", address: "0xB5C064F955D8e7F38fE0460C556a72987494eE17", decimals: 18 },
-  { name: "LINK",  address: "0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39", decimals: 18 }
+  { name: "PEPE",    address: "0x17215aA142591f4456E17C8947B8E7a555721C4F", decimals: 18 },
+  { name: "TURBO",   address: "0x3b4756d373C354E4863E4C9181431De7A671573D", decimals: 18 },
+  { name: "MOCHI",   address: "0x5D35d8E83067633152A6192158C943Af78919501", decimals: 18 },
+  { name: "FLOKI",   address: "0x8c71e2561F6c2F872438B5C8C1813aE19a28E6c9", decimals: 18 },
+  { name: "WOJAK",   address: "0x8d1E726931B238D2f84d6923f0C2E125E716C792", decimals: 18 },
+  { name: "CAT",     address: "0x1a8e39ae59e55c2029313c536f2f30e49D8c3f8A", decimals: 18 },
+  { name: "DOGE",    address: "0xb7b31edF4C1A7a2122A9829E6aF1F6D7aB6Cf0F9", decimals: 18 },
+  { name: "SHIB",    address: "0x431553B2dF3F292557EEeF55B7C2c2C5F7780354", decimals: 18 },
+  { name: "MILK",    address: "0x5a435213a3D460EcD1D114E82A0025c0251AaD7B", decimals: 18 },
+  { name: "BONK",    address: "0x6Bd6F08B6226A55A7E264E1A51a3A666c26A9777", decimals: 18 },
+  { name: "WIF",     address: "0x4B23A71C7C1a1D9E4E7c1E8c3f4A8D7B9F0E1C2D", decimals: 18 }, // Dogwifhat (WIF) on Polygon
+  { name: "POPCAT",  address: "0x2a6f7E7F8C3B4D5E6F7A8B9C0D1E2F3A4B5C6D7E", decimals: 18 },
+  { name: "NEIRO",   address: "0x9F8E7D6C5B4A3C2B1A0F9E8D7C6B5A4F3E2D1C0B", decimals: 18 },
+  { name: "MYRO",    address: "0x1F2E3D4C5B6A7F8E9D0C1B2A3F4E5D6C7B8A9F0E", decimals: 18 },
+  { name: "BRETT",   address: "0xA1B2C3D4E5F6A7B8C9D0E1F2A3B4C5D6E7F8A9B0", decimals: 18 },
+  { name: "SMOL",    address: "0xC1D2E3F4A5B6C7D8E9F0A1B2C3D4E5F6A7B8C9D0", decimals: 18 },
+  { name: "PENGU",   address: "0xD1E2F3A4B5C6D7E8F9A0B1C2D3E4F5A6B7C8D9E0", decimals: 18 },
+  { name: "FARTCOIN",address: "0xE1F2A3B4C5D6E7F8A9B0C1D2E3F4A5B6C7D8E9F0", decimals: 18 },
+  { name: "MOTHER",  address: "0xF1E2D3C4B5A6F7E8D9C0B1A2F3E4D5C6B7A8F9E0", decimals: 18 },
+  { name: "TRUMP",   address: "0x0A1B2C3D4E5F6A7B8C9D0E1F2A3B4C5D6E7F8A9B", decimals: 18 }
 ];
